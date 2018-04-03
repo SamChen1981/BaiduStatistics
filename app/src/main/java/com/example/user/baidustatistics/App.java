889c5f62998d3c822e69f1567bfad969f4eb580e
wei <deva1213082309@gmail.com>
2018-04-03 10:19:47 +0800

    first commit

---
+++ app/src/main/java/com/example/user/baidustatistics/App.java
@@ -0,0 +1,16 @@
+package com.example.user.baidustatistics;
+
+import android.app.Application;
+
+/**
+ * Created by zitan on 2018/4/2.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
---
+++ app/src/main/java/com/example/user/baidustatistics/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.user.baidustatistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+}
